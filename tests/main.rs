2d149ca2676e8361b7dee43112713fe3d6c0b818
Alex Aunter <lxaunter@gmail.com>
2022-02-05 13:19:55 +0300

    test(view): add validate schedule method tests

--- tests/main.rs
+++ tests/main.rs
@@ -810,6 +810,33 @@ fn test_lockup_terminate_custom_vesting_incompatible_vesting_schedule_by_hash() 
 }
 
 #[test]
+fn test_validate_schedule() {
+    let e = Env::init(None);
+    let users = Users::init(&e);
+    let amount = d(60000, TOKEN_DECIMALS);
+    e.set_time_sec(GENESIS_TIMESTAMP_SEC);
+    let lockups = e.get_account_lockups(&users.alice);
+    assert!(lockups.is_empty());
+
+    let (lockup_schedule, vesting_schedule) = lockup_vesting_schedule(amount);
+
+    let res = e.validate_schedule(&lockup_schedule, amount.into(), Some(&vesting_schedule));
+    assert!(res.is_ok());
+
+    let incompatible_vesting_schedule = Schedule(vec![
+        Checkpoint { timestamp: GENESIS_TIMESTAMP_SEC + ONE_YEAR_SEC * 4, balance: 0 },
+        Checkpoint { timestamp: GENESIS_TIMESTAMP_SEC + ONE_YEAR_SEC * 4 + 1, balance: amount },
+    ]);
+    let res = e.validate_schedule(
+        &lockup_schedule,
+        amount.into(),
+        Some(&incompatible_vesting_schedule),
+    );
+    assert!(!res.is_ok());
+    assert!(format!("{:?}", res.unwrap_err()).contains("The lockup schedule is ahead of"));
+}
+
+#[test]
 fn test_lockup_terminate_custom_vesting_terminate_before_cliff() {
     let e = Env::init(None);
     let users = Users::init(&e);
